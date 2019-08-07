5ef855f23c6555df7d9fd6d86108f05763c0b456
Zdravko Kolev <ZKolev@infragistics.com>
2019-08-07 14:02:22 +0300

    Local data must be within the app; Updating readme

--- Grid/FinJS/src/app/grid-finjs/localData.service.ts
+++ Grid/FinJS/src/app/grid-finjs/localData.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable} from 'rxjs';
-import { FinancialData } from '../../../../../LocalData/financialData';
+import { FinancialData } from '../../localData/financialData';
 
 @Injectable()
 export class LocalDataService {
